c6a1a59b0b7481157b48199d805762a43f25c97e
software-1234 <irishswedens@gmail.com>
2017-10-14 15:16:19 -0400

    start of something new

---
+++ app.py
@@ -0,0 +1,20 @@
+import os
+import sys
+
+sys.path.insert(1, os.path.join(os.path.abspath('.'), 'lib'))
+import logging
+from flask import json,Flask,render_template,request,jsonify
+
+#import api_yelp
+#import parse
+app = Flask(__name__)
+
+
+@app.route('/')
+def start():
+    return render_template('index.html')
+
+@app.route('/api/search')
+def search():
+    return render_template('form.html')
+
