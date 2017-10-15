780aa4f212b8edba5803105c181d50fd3ace7817
James Jushchuk <jhj3@live.unc.edu>
2017-10-15 07:02:39 -0400

    fix bugs

--- distance.py
+++ distance.py
@@ -2,7 +2,7 @@ import database
 import googlemaps
 def getDistance():	
 	my_latitudes=database.get_valid_locations()
-	print my_latitudes.length
+	print(len(my_latitudes))
 	gmaps = googlemaps.Client(key='AIzaSyApuvnawK-aqRhvTW9tsLoKJGT-24fPrvY')
       
 	
--- message_maker.py
+++ message_maker.py
@@ -1,12 +1,11 @@
 def getReply(message):
-
     message = message.lower().strip() 
     answer = ""
 
     if "latest transaction" or "recent transaction" in message:
-	answer  = "here is your latest transaction"
+        answer  = "here is your latest transaction"
     elif "near me" in message:
-	answer = "the closest food bank near you is 2 miles away."
+        answer = "the closest food bank near you is 2 miles away."
     else:
-	answer = "please try another question?"
+        answer = "please try another question?"
     return answer
