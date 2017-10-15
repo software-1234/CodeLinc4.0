f83e086513418e2012a93e1f5e9fe065bc7bf88a
James Jushchuk <jhj3@live.unc.edu>
2017-10-15 08:35:59 -0400

    first run of twilio logic

--- distance.py
+++ distance.py
@@ -1,8 +1,38 @@
 import database
 import googlemaps
-def getDistance():	
-	my_latitudes=database.get_valid_locations()
-	print(len(my_latitudes))
-	gmaps = googlemaps.Client(key='AIzaSyApuvnawK-aqRhvTW9tsLoKJGT-24fPrvY')
+import decimal
+import math
+
+def get_distance(latlng, filter):	
+    D = decimal.Decimal
+    res = []
+    if filter == "all":
+        res = database.get_valid_locations_non_json()
+    else:
+        res = database.get_valid_locations_by_type(filter)
+    #gmaps = googlemaps.Client(key='AIzaSyApuvnawK-aqRhvTW9tsLoKJGT-24fPrvY')
+
+    lat2 = latlng[0]
+    lon2 = latlng[1]
+
+    R = 6373.0
+    min_row = None
+    min_distance = 0
+    for r in res:
+        lat1 = math.radians(D(r[17]))
+        lon1 = math.radians(D(r[18]))
+        dlon = lon2 - lon1
+        dlat = lat2 - lat1
+
+        a = math.sin(dlat / 2)**2 + math.cos(lat1) * math.cos(lat2) * math.sin(dlon / 2)**2
+        c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+
+        distance = R * c
+        if distance > min_distance:
+            print(distance)
+            min_distance = distance
+            min_row = r
+        
+    return (min_distance,min_row)
       
 	
--- message_maker.py
+++ message_maker.py
@@ -1,11 +1,50 @@
-def getReply(message):
+import distance
+import database
+
+def getReply(message, from_number):
     message = message.lower().strip() 
     answer = ""
 
-    if "latest transaction" or "recent transaction" in message:
+    if "transaction" in message:
         answer  = "here is your latest transaction"
-    elif "near me" in message:
-        answer = "the closest food bank near you is 2 miles away."
+    elif " near " in message:
+        typeOfResource = message.split("near",1)[0]
+        location = message.split("near",1)[1]
+        catagory = ""
+        if "food pantry" in typeOfResource:
+            catagory = "Food Pantry"
+        elif "summer meal" in typeOfResource:
+            catagory = "Summer Meals"
+        elif "education" in typeOfResource or "school" in typeOfResource:
+            catagory = "Educational Resource"
+        elif "free meal" in typeOfResource or "meal" in typeOfResource:
+            catagory = "Free Meal"
+        elif "convenience strore" in typeOfResource:
+            catagory = "Convenience Store"
+        elif "garden" in typeOfResource:
+            catagory = "Community Garden"
+        elif "backpack" in typeOfResource:
+            catagory = "BackPack Program"
+        elif "grocery" in typeOfResource or "coop" in typeOfResource or "co-op" in typeOfResource:
+            catagory = "Grocery/Co-Op"
+        elif "farm" in typeOfResource or "csa" in typeOfResource:
+            catagory = "CSA Program"
+        elif "mobile" in typeOfResource:
+            catagory = "Mobile market"
+        else:
+            catagory = "all"
+        
+        latlng = database.get_home_address_by_phone(from_number)
+        result = distance.get_distance(latlng, catagory)
+        print(catagory)
+        
+        answer = "Closest "+ result[1][3] +" is "+result[1][1]+" on "+result[1][8]+", "+result[1][9]+". "
+        if result[1][7] != "":
+            answer += "Phone # is " +str(result[1][7]) + ". "
+        if result[1][12] != "":
+            answer += "Open on " +str(result[1][12]) + " "
+        if result[1][12] != "":
+            answer += " " +str(result[1][13])
     else:
         answer = "please try another question?"
     return answer
