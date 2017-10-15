94651cc2efc656973c81c3dc6cbecabaab3e617c
software-1234 <irishswedens@gmail.com>
2017-10-15 01:10:39 -0400

    added basic text templates

---
+++ message_maker.py
@@ -0,0 +1,12 @@
+def getReply(message):
+
+    message = message.lower().strip() 
+    answer = ""
+
+    if "latest transaction" or "recent transaction" in message:
+	answer  = "here is your latest transaction"
+    elif "near me" in message:
+	answer = "the closest food bank near you is 2 miles away."
+    else:
+	answer = "please try another question?"
+    return answer
